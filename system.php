73056f68073f7fd1f3ad2ceba94d3662f994fba6
Olivier Dalang <olivier.dalang@epfl.ch>
2014-12-11 11:42:00 +0100

    test composer

---
+++ system.php
@@ -0,0 +1,14 @@
+<?php
+namespace olivierdalang\mvc2000;
+/**
+ * A terribly useful class
+ *
+ * See: http://knpuniversity.com/screencast/question-answer-day
+ */
+class System
+{
+    public function sayHallo()
+    {
+        return 'Hallo!';
+    }
+}
