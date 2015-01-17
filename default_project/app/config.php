9d956f5a5b403f397471dce7d79f13888ab69e41
olivierdalang <olivier.dalang@gmail.com>
2015-01-17 12:20:24 +0000

    added a configuration array

--- default_project/app/config.php
+++ default_project/app/config.php
@@ -3,6 +3,7 @@
 use \olivierdalang\mvc2000\Sys;
 
 Sys::$b = '/'; 						//holds the base url as a string, usefull to create absolute urls...
+Sys::$c['debug'] = 'false'; 		//an example custom configuration variable
 Sys::$defaultUri = 'page/index';	//default controller/method when none is specified
 
 //modules to load
